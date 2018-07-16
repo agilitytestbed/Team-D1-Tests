b5f2b96c8466e0cf69574c3c9a13b3afeb0ede58
Daan Kooij <d.kooij-1@student.utwente.nl>
2018-07-16 10:23:07 +0200

    Create TestSuite that calls all tests

---
+++ src/main/java/nl/utwente/ing/testing/TestSuite.java
@@ -0,0 +1,15 @@
+package nl.utwente.ing.testing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+        InitialSystemTest.class,
+        CategoryRulesTest.class,
+        BalanceHistoryTest.class
+})
+public class TestSuite {
+
+}
